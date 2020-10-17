1f39a7056c5fd3d04dc2937f42e7c45634184a12
walery0001 <marcin.walery@gmail.com>
2020-10-17 16:50:44 +0200

    masterIndex

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnTop = document.getElementById('btnTop');
+const topUl = document.querySelector('.topUl');
+
+btnTop.addEventListener('click', () =>{
+topUl.classList.toggle('showTopUl');
+});
+
+// window.addEventListener('click', (e)=>{
+// const quelew = e.target;
+// console.log(quelew);
+// })
+
+
