8f2c6cc2d7f5d0b7cd0e4355bc208a52d21fa939
walery0001 <marcin.walery@gmail.com>
2020-10-19 21:41:09 +0200

    leftJS

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const btnTop = document.getElementById('btnTop');
 const topUl = document.querySelector('.topUl');
+const leftUl = document.querySelector('.leftNav');
 
 btnTop.addEventListener('click', () =>{
 topUl.classList.toggle('showTopUl');
 });
 
+window.addEventListener('click',(e)=>{
+    if(e.target === leftUl)
+    leftUl.classList.toggle('showLeftNav')
+console.log(e.target)
+})
 // window.addEventListener('click', (e)=>{
 // const quelew = e.target;
 // console.log(quelew);
