0b080fadc791abac08a88cf496e4ce11a4b68bb7
walery0001 <marcin.walery@gmail.com>
2020-10-20 18:45:26 +0200

    left Nav ready

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const btnTop = document.getElementById('btnTop');
 const topUl = document.querySelector('.topUl');
-const leftUl = document.querySelector('.leftNav');
+const leftNav = document.querySelector('.leftNav');
+const arrowLeft=document.getElementById('arrowLeft');
+const leftX =document.getElementById('leftX');
 
 btnTop.addEventListener('click', () =>{
 topUl.classList.toggle('showTopUl');
 });
 
+leftX.addEventListener('click', () =>{
+leftNav.classList.remove('showLeftNav');
+});
+
+arrowLeft.addEventListener('click', () =>{
+leftNav.classList.add('showLeftNav');
+});
+
 window.addEventListener('click',(e)=>{
-    if(e.target === leftUl)
-    leftUl.classList.toggle('showLeftNav')
-console.log(e.target)
-})
+    if(e.target === leftNav){
+    	 leftNav.classList.remove('showLeftNav')
+}
+    }
+   
+)
+
 // window.addEventListener('click', (e)=>{
 // const quelew = e.target;
 // console.log(quelew);
 // })
-
-
